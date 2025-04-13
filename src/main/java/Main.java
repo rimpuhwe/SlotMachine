580b70b64bf9601fc612522f847d5d990f97c22e
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-04-13 11:41:20 +0200

    printing the rows , spinning , and get the payout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,14 @@
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Random;
-import java.util.Scanner;
+
+import java.util.*;
 
 public class Main {
     static String[] spin ={"\uD83C\uDF49" , "\uD83C\uDF4B" , "\uD83D\uDD14" , "\uD83C\uDF4F"};
-    static int reward;
     public static void main(String[] args) throws InterruptedException {
         int balance = 1000 ;
         int bet;
+        String[] row ;
+        int payout;
+        String playAgain = "Yes";
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Here is the slot machine , Let's play and earn rewards!");
@@ -21,17 +21,42 @@ public class Main {
             showSpin();
             while(balance > 0){
                 Thread.sleep(2000);
+                System.out.println("current balance : $" + balance);
                 System.out.print("Place your bet: ");
                 bet = sc.nextInt();
-                if(bet > balance || bet < 0){
-                    System.out.println("you can't bet a negative amount  or place a bet which exceeds your balance");
+                sc.nextLine();
+                if(bet > balance ){
+                    System.out.println("INSUFFICIENT BALANCE");
+                    continue;
                 }
-                balance -= bet;
-                spin();
-                System.out.println("The remaining  balance : " + balance);
+                else if(bet <= 0){
+                    System.out.println("Bet must be greater than 0 , not negative one ");
+                    continue;
+                }
+                else{
+                    balance -= bet;
+                   row =  spin();
 
-
-
+                    System.out.println("Spinning .....");
+                    Thread.sleep(2500);
+                    System.out.println("*************");
+                    printRow(row);
+                    System.out.println("*************");
+                    payout = getPayout(row , bet);
+                    if (payout > 0){
+                        System.out.println("you win this round");
+                        System.out.println("payout : $" + payout);
+                        balance += payout;
+                    }
+                    else{
+                        System.out.println("sorry , you lost this round");
+                    }
+                    System.out.print("Do you want to continue playing? (Y/N) :");
+                    playAgain = sc.nextLine();
+                    if(!playAgain.equalsIgnoreCase("Y")){
+                        break;
+                    }
+                }
 
             }
 
@@ -44,9 +69,10 @@ public class Main {
         else {
             System.out.println("Invalid input");
         }
+        System.out.println("GAME OVER , your final balance : $" + balance);
 
     }
-    public static void showSpin() {
+    static void showSpin() {
         System.out.println("*************");
         System.out.print("Our spinners : ");
         for (String s : spin) {
@@ -55,38 +81,50 @@ public class Main {
         System.out.println("\n***********");
 
     }
-    public static void spin(){
+    static String[] spin(){
         Random random = new Random();
         String[] newSpin = new String[3];
         for (int i = 0; i < 3; i++) {
             newSpin[i] = spin[random.nextInt(spin.length)];
         }
-        for(String s : newSpin){
-            System.out.print(s + " ");
-            System.out.println();
-        }
-        if (Objects.equals(newSpin[0], newSpin[1]) && Objects.equals(newSpin[1], newSpin[2])) {
-            System.out.println("*******");
-            System.out.println("you win");
-            System.out.println("*******");
-            reward +=  50;
-            System.out.println("reward: " + reward);
-
-
-        }
-        else if(Objects.equals(newSpin[0], newSpin[1]) || Objects.equals(newSpin[1], newSpin[2]) || Objects.equals(newSpin[0], newSpin[2])){
-            System.out.println("*******");
-            System.out.println("you almost win");
-            System.out.println("*******");
-            reward +=  20;
-            System.out.println("reward: " + reward);
-        }
-        else{
-            System.out.println("*******");
-            System.out.println("you lost");
-            System.out.println("*******");
-
-        }
+        return newSpin;
 
     }
+    static  void printRow(String[] row){
+        System.out.println(" " + String.join(" | ", row));
+    }
+    static int getPayout(String[] row , int bet) {
+        if (row[0].equals(row[1]) && row[1].equals(row[2])) {
+           return  switch (row[0]) {
+               case "\uD83C\uDF49" -> bet * 2;
+               case "\uD83C\uDF4B" -> bet * 4;
+               case "\uD83D\uDD14" -> bet * 6;
+               case "\uD83C\uDF4F" -> bet * 10;
+               default ->  0;
+
+           };
+        }
+        else if (row[0].equals(row[1])) {
+            return  switch (row[0]) {
+                case "\uD83C\uDF49" -> bet * 2;
+                case "\uD83C\uDF4B" -> bet * 3;
+                case "\uD83D\uDD14" -> bet * 4;
+                case "\uD83C\uDF4F" -> bet * 5;
+                default ->  0;
+
+            };
+        }
+        else if (row[1].equals(row[2])) {
+            return  switch (row[1]) {
+                case "\uD83C\uDF49" -> bet * 2;
+                case "\uD83C\uDF4B" -> bet * 3;
+                case "\uD83D\uDD14" -> bet * 4;
+                case "\uD83C\uDF4F" -> bet * 5;
+                default ->  0;
+
+            };
+        }
+        return 0;
+    }
+
 }
